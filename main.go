4b7cc9909bfdec06b298741672614a46594410f5
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2022-12-20 16:35:09 +0700

    add staff methods & edit main.go

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func main() {
 	var productMenu = product.ProductMenu{DB: conn}
 
 	var inputMenu = 1
+
 	for inputMenu != 0 {
 		fmt.Println("Welcome To Tokoku")
 		fmt.Print("1. Login\n0. Exit\nInsert Your Menu : ")
@@ -172,7 +173,8 @@ func main() {
 
 						}
 					}
-				} else if res == 1 {
+				}
+			} else if res.ID == 1 {
 					islogin := true
 					for islogin {
 						fmt.Println("Admin Menu")
@@ -187,22 +189,39 @@ func main() {
 						fmt.Scanln(&choice)
 						switch choice {
 						case 1:
-							{
-								fmt.Println("add a new staff")
-
+							var newStaff staff.Staff
+							fmt.Println("\n", "== Insert New Staff ==")
+							fmt.Print("Masukkan nama: ")
+							fmt.Scanln(&newStaff.Name)
+							fmt.Print("Masukkan password: ")
+							fmt.Scanln(&newStaff.Password)
+							res, err := staffMenu.Register(newStaff)
+							if err != nil {
+								fmt.Println(err.Error())
+							}
+							if res {
+								fmt.Println("Sukses mendaftarkan data")
+							} else {
+								fmt.Println("Gagal mendaftarn data")
 							}
 						case 2:
-							{
-								fmt.Println("remove a staff")
+							var removeStaff staff.Staff
+							fmt.Println("\n", "== Remove Staff ==")
+							fmt.Print("Staff name: ")
+							fmt.Scanln(&removeStaff.Name)
+							res, err := staffMenu.Remove(removeStaff.Name)
+							if err != nil {
+								fmt.Println(err.Error())
+							}
+							if res {
+								fmt.Println("Berhasil menghapus data")
+							} else {
+								fmt.Println("Gagal menghapus data")
 							}
 						case 3:
-							{
-								fmt.Println("remove product")
-							}
+							fmt.Println("remove product")
 						case 4:
-							{
-								fmt.Println("remove transaction")
-							}
+							fmt.Println("remove transaction")
 						case 5:
 							{
 								var namacus string
@@ -219,15 +238,11 @@ func main() {
 									fmt.Println("Sorry Can't Delete Customer")
 								}
 							}
+							fmt.Println("remove customer")
 						case 9:
-							{
-								fmt.Println("bye")
-								islogin = false
-							}
-						}
-
+							fmt.Println("bye")
+							islogin = false
 					}
-
 				}
 			}
 		}
--- staff/staff.go
+++ staff/staff.go
@@ -18,13 +18,152 @@ type Staff struct {
 	UpdatedDate string
 }
 
-func (sm *StaffMenu) Login(nama string, password string) (int, error) {
-	selectid := sm.DB.QueryRow("SELECT id_staff FROM staffs WHERE name = ? AND password = ?", nama, password)
-	var IDada int
-	err := selectid.Scan(&IDada)
+func (sm *StaffMenu) Login(name string, password string) (Staff, error) {
+	// selectid := sm.DB.QueryRow("SELECT id_staff FROM staffs WHERE name = ? AND password = ?", nama, password)
+	// var IDada int
+	// err := selectid.Scan(&IDada)
+	// if err != nil {
+	// 	log.Println("error saat select dari database", err.Error())
+	// 	return 0, errors.New("password atau username salah")
+	// }
+	// return IDada, nil
+
+	loginQry, err := sm.DB.Prepare("SELECT id_staff FROM staffs WHERE name = ? AND password = ?")
 	if err != nil {
-		log.Println("error saat select dari database", err.Error())
-		return 0, errors.New("password atau username salah")
+		log.Println("prepare insert staff ", err.Error())
+		return Staff{}, errors.New("prepare statement insert staff error")
 	}
-	return IDada, nil
+
+	row := loginQry.QueryRow(name, password)
+
+	if row.Err() != nil {
+		log.Println("login query ", row.Err().Error())
+		return Staff{}, errors.New("tidak bisa login, data tidak ditemukan")
+	}
+	res := Staff{}
+	err = row.Scan(&res.ID)
+
+	if err != nil {
+		log.Println("after login query ", err.Error())
+		return Staff{}, errors.New("tidak bisa login, kesalahan setelah error")
+	}
+
+	res.Name = name
+
+	return res, nil
+}
+
+func (sm *StaffMenu) Duplicate(name string) bool {
+	res := sm.DB.QueryRow("SELECT id_staff FROM staffs where name = ?", name)
+	var idExist int
+	err := res.Scan(&idExist)
+	if err != nil {
+		if err.Error() != "sql: no rows in result set" {
+			log.Println("Result scan error", err.Error())
+        }
+        return false
+    }
+	return true
+}
+
+func (sm *StaffMenu) Register(newStaff Staff) (bool, error) {
+	// menyiapakn query untuk insert
+	registerQry, err := sm.DB.Prepare("INSERT INTO staffs (name, password, created_date, updated_date) values (?, ?, now(), null)")
+	if err != nil {
+		log.Println("prepare insert staff ", err.Error())
+		return false, errors.New("prepare statement insert staff error")
+	}
+	
+	if sm.Duplicate(newStaff.Name) {
+		log.Println("duplicated information")
+		return false, errors.New("nama sudah digunakan")
+	}
+	
+	// menjalankan query dengan parameter tertentu
+	res, err := registerQry.Exec(newStaff.Name, newStaff.Password)
+	if err != nil {
+		log.Println("insert staff ", err.Error())
+		return false, errors.New("insert staff error")
+	}
+	// Cek berapa baris yang terpengaruh query diatas
+	affRows, err := res.RowsAffected()
+	
+	if err != nil {
+		log.Println("after insert staff ", err.Error())
+		return false, errors.New("error setelah insert")
+	}
+	
+	if affRows <= 0 {
+		log.Println("no record affected")
+		return false, errors.New("no record")
+	}
+	
+	return true, nil
+}
+
+func (sm *StaffMenu) Show() ([]Staff, error) {
+	rows, err := sm.DB.Query("SELECT id_staff, name FROM staffs")
+	if err != nil {
+    	log.Println(err)
+	}
+
+	res := []Staff{} // creating empty slice
+	defer rows.Close()
+
+	for rows.Next() {
+		staff := Staff{} // creating new struct for every row
+		err = rows.Scan(&staff.ID, &staff.Name)
+		if err != nil {
+			log.Println(err)
+		}
+		res = append(res, staff)
+	}
+
+	return res, nil
+}
+
+// func (sm *StaffMenu) Check(name string) bool {
+// 	res := sm.DB.QueryRow("SELECT name FROM staffs where name = ?", name)
+// 	var admin string = "admin"
+// 	err := res.Scan(&admin)
+// 	if err != nil {
+// 		if err.Error() != "sql: no rows in result set" {
+// 			log.Println("Result scan error", err.Error())
+//         }
+//         return false
+//     }
+// 	return true
+// }
+
+func (sm *StaffMenu) Remove(name string) (bool, error) {
+	removeQry, err := sm.DB.Prepare("DELETE FROM staffs WHERE name = ?")
+	if err != nil {
+		log.Println("prepare remove staff ", err.Error())
+		return false, errors.New("prepare statement remove staff error")
+	}
+
+	// if sm.Check(name) {
+	// 	log.Println("admin information")
+	// 	return false, errors.New("admin tidak dapat dihapus")
+	// }
+
+	res, err := removeQry.Exec(name)
+	if err != nil {
+		log.Println("remove staff ", err.Error())
+		return false, errors.New("remove staff error")
+	}
+
+	affRows, err := res.RowsAffected()
+	
+	if err != nil {
+		log.Println("after remove staff ", err.Error())
+		return false, errors.New("error setelah remove")
+	}
+	
+	if affRows <= 0 {
+		log.Println("no record affected")
+		return false, errors.New("no record")
+	}
+	
+	return true, nil
 }
