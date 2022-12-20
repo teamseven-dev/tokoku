ed7aa00521a6ba2a99a2c858d8e067afb998476b
indra <webhosting534@gmail.com>
2022-12-20 10:26:02 +0700

    add login feature

--- main.go
+++ main.go
@@ -3,11 +3,33 @@ package main
 import (
 	"fmt"
 	"tokoku/config"
+	"tokoku/staff"
 )
 
 func main() {
 	var cfg = config.ReadConfig()
 	var conn = config.ConnectSQL(*cfg)
+	var staffMenu = staff.StaffMenu{DB: conn}
 
 	fmt.Println(conn)
+	var inputMenu = 1
+	for inputMenu != 0 {
+		fmt.Println("Welcome To Tokoku")
+		fmt.Print("1. Login\n0. Exit\nInsert Your Menu :")
+		fmt.Scanln(&inputMenu)
+		if inputMenu == 1 {
+			var inputName, inputPassword string
+			fmt.Print("Username :")
+			fmt.Scanln(&inputName)
+			fmt.Print("Password :")
+			fmt.Scanln(&inputPassword)
+			res, err := staffMenu.Login(inputName, inputPassword)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			if res > 0 {
+				fmt.Println("Success Login ")
+			}
+		}
+	}
 }
