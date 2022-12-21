e8b21e13aa9e1c60fa7227c1d494e963b7837eec
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2022-12-21 09:40:15 +0700

    add feature: can't remove admin

--- staff/staff.go
+++ staff/staff.go
@@ -47,3 +47,77 @@ func (sm *StaffMenu) Login(name string, password string) (Staff, error) {
 
 	return res, nil
 }
+
+// func (sm *StaffMenu) Check(name string) bool {
+// 	res := sm.DB.QueryRow("SELECT name FROM staffs where name = ?", name)
+// 	var admin string = "admin"
+// 	err := res.Scan(&admin)
+// 	if err != nil {
+// 		if err.Error() != "sql: no rows in result set" {
+// 			log.Println("Result scan error", err.Error())
+//         }
+//         return false
+//     }
+// 	return true
+// }
+
+func (sm *StaffMenu) Remove(name string) (bool, error) {
+	removeQry, err := sm.DB.Prepare("DELETE FROM staffs WHERE name = ?")
+	if err != nil {
+		log.Println("prepare remove staff ", err.Error())
+		return false, errors.New("prepare statement remove staff error")
+	}
+
+	// if sm.Check(name) {
+	// 	log.Println("admin information")
+	// 	return false, errors.New("admin tidak dapat dihapus")
+	// }
+
+	res, err := removeQry.Exec(name)
+	if err != nil {
+		log.Println("remove staff ", err.Error())
+		return false, errors.New("remove staff error")
+	}
+
+	affRows, err := res.RowsAffected()
+	
+	if err != nil {
+		log.Println("after remove staff ", err.Error())
+		return false, errors.New("error setelah remove")
+	}
+	
+	if affRows <= 0 {
+		log.Println("no record affected")
+		return false, errors.New("no record")
+	}
+	
+	return true, nil
+}
+
+func (sm *StaffMenu) UpdateStaff(updateName string, updatePassword string, id int) (bool, error) {
+	updateStaffQry, err := sm.DB.Prepare("UPDATE staffs SET name = ? AND password = ? WHERE id_staff = ?")
+	if err != nil {
+		log.Println("prepare update name and password ", err.Error())
+		return false, errors.New("prepare statement update name and password error")
+	}
+
+	res, err := updateStaffQry.Exec(updateName, updatePassword)
+	if err != nil {
+		log.Println("update name and password ", err.Error())
+		return false, errors.New("update name and password error")
+	}
+	// Cek berapa baris yang terpengaruh query diatas
+	affRows, err := res.RowsAffected()
+
+	if err != nil {
+		log.Println("after update name and password ", err.Error())
+		return false, errors.New("error setelah update name and password")
+	}
+
+	if affRows <= 0 {
+		log.Println("no record affected")
+		return false, errors.New("no record")
+	}
+
+	return true, nil
+}
